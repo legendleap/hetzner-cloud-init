801b57205d8b3be00a8f542469f9c76ac845a0cd
B-OnTheGo <42626718+beeonthego@users.noreply.github.com>
2020-09-08 13:45:56 +1000

    Create setup-behind-proxy.sh
    setup ufw and one-off update account ips

---
+++ setup-behind-proxy.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+  --hcloud-token)
+    TOKEN="$2"
+    shift
+    shift
+  ;;
+  --whitelisted-ips)
+    WHITELIST_S="$2"
+    shift
+    shift
+  ;;
+  --floating-ips)
+    FLOATING_IPS="--floating-ips"
+    shift
+  ;;
+  *)
+    shift
+  ;;
+esac
+done
+
+FLOATING_IPS=${FLOATING_IPS:-""}
+
+
+sed -i 's/[#]*PermitRootLogin yes/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+sed -i 's/[#]*PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+
+systemctl restart sshd
+
+wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x jq-linux64
+mv jq-linux64 /usr/local/bin/jq
+
+
+curl -o /usr/local/bin/update-config.sh https://raw.githubusercontent.com/legendleap/hetzner-cloud-init/bocs/update-config.sh
+
+chmod +x /usr/local/bin/update-config.sh
+
+ufw allow proto tcp from any to any port 22
+# ufw allow proto tcp from any to any port 22,110,995,143,993,25,465,587 #for email node
+
+ufw -f enable
+
+IFS=', ' read -r -a WHITELIST <<< "$WHITELIST_S"
+
+for IP in "${WHITELIST[@]}"; do
+  ufw allow from "$IP"
+done
+
+ufw allow from 10.43.0.0/16
+ufw allow from 10.42.0.0/16
+ufw allow from 10.0.0.0/16 # default private network cidr
+ufw allow from 10.244.0.0/16 # in case we use the default cidr expected by the cloud controller manager
+ufw allow from 10.1.0.0/16
+
+ufw -f default deny incoming
+ufw -f default allow outgoing
+
+# cat <<EOF >> /etc/crontab
+# * * * * * root /usr/local/bin/update-config.sh --hcloud-token ${TOKEN} --whitelisted-ips ${WHITELIST_S} ${FLOATING_IPS}
+# EOF
+
+/usr/local/bin/update-config.sh --hcloud-token ${TOKEN} --whitelisted-ips ${WHITELIST_S} ${FLOATING_IPS}
